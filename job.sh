c827ab2d0fd2fc634f54f647d925da065ad1a2e1
Robert Zacchia <robert.zacchia@students.uibk.ac.at>
2026-03-19 17:14:50 +0100

    cc3 resuts

--- job.sh
+++ job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --partition=lva
-#SBATCH --job-name ex01
+#SBATCH --job-name ex03
 #SBATCH --output=%j_job_output.log
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
@@ -15,15 +15,14 @@ module load python/3.10.8-gcc-8.5.0-r5lf3ij # a newer version of python
 
 echo ========== Starting building ================
 mkdir -p ~/perf-oriented-dev/larger_samples/npb_bt/build
-cd ~/perf-oriented-dev/larger_samples/npb_bt/
-cd build
+cd ~/perf-oriented-dev/larger_samples/npb_bt/build
 cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release
 ninja
 
 ./npb_bt_a
 mv gmon.out gmon_a.out
-gprof ./npb_bt_a gmon_a.out > profile_a.txt
+gprof ./npb_bt_a gmon_a.out > ~/perf-oriented-dev/profile_a.txt
 
 ./npb_bt_b
 mv gmon.out gmon_b.out
-gprof ./npb_bt_b gmon_b.out > profile_b.txt
+gprof ./npb_bt_b gmon_b.out > ~/perf-oriented-dev/profile_b.txt
