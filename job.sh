d8b93ed01332c4c0f4d80bfbd512a032a3446c5a
Robert Zacchia <robert.zacchia@students.uibk.ac.at>
2026-03-22 12:38:36 +0100

    llcc3 results

--- job.sh
+++ job.sh
@@ -19,10 +19,43 @@ cd ~/perf-oriented-dev/larger_samples/npb_bt/build
 cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release
 ninja
 
+
 ./npb_bt_a
 mv gmon.out gmon_a.out
-gprof ./npb_bt_a gmon_a.out > ~/perf-oriented-dev/profile_a.txt
+gprof ./npb_bt_a gmon_a.out > profile_a_lcc3_1.txt
+
+./npb_bt_a
+mv gmon.out gmon_a.out
+gprof ./npb_bt_a gmon_a.out > profile_a_lcc3_2.txt
+
+./npb_bt_a
+mv gmon.out gmon_a.out
+gprof ./npb_bt_a gmon_a.out > profile_a_lcc3_3.txt
+
+./npb_bt_a
+mv gmon.out gmon_a.out
+gprof ./npb_bt_a gmon_a.out > profile_a_lcc3_4.txt
+
+./npb_bt_a
+mv gmon.out gmon_a.out
+gprof ./npb_bt_a gmon_a.out > profile_a_lcc3_5.txt
 
 ./npb_bt_b
 mv gmon.out gmon_b.out
-gprof ./npb_bt_b gmon_b.out > ~/perf-oriented-dev/profile_b.txt
+gprof ./npb_bt_b gmon_b.out > profile_b_lcc3_1.txt
+
+./npb_bt_b
+mv gmon.out gmon_b.out
+gprof ./npb_bt_b gmon_b.out > profile_b_lcc3_2.txt
+
+./npb_bt_b
+mv gmon.out gmon_b.out
+gprof ./npb_bt_b gmon_b.out > profile_b_lcc3_3.txt
+
+./npb_bt_b
+mv gmon.out gmon_b.out
+gprof ./npb_bt_b gmon_b.out > profile_b_lcc3_4.txt
+
+./npb_bt_b
+mv gmon.out gmon_b.out
+gprof ./npb_bt_b gmon_b.out > profile_b_lcc3_5.txt
