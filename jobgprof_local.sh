57b08a66b322960d8eded2c543ab7284ed1e2934
Robert Zacchia <robert.zacchia@students.uibk.ac.at>
2026-03-22 12:22:05 +0100

    local runs

--- jobgprof_local.sh
+++ jobgprof_local.sh
@@ -9,8 +9,32 @@ ninja
 
 ./npb_bt_a
 mv gmon.out gmon_a.out
-gprof ./npb_bt_a gmon_a.out > profile_a.txt
+gprof ./npb_bt_a gmon_a.out > profile_a_local_1.txt
+
+./npb_bt_a
+mv gmon.out gmon_a.out
+gprof ./npb_bt_a gmon_a.out > profile_a_local_2.txt
+
+./npb_bt_a
+mv gmon.out gmon_a.out
+gprof ./npb_bt_a gmon_a.out > profile_a_local_3.txt
+
+./npb_bt_a
+mv gmon.out gmon_a.out
+gprof ./npb_bt_a gmon_a.out > profile_a_local_4.txt
 
 ./npb_bt_b
 mv gmon.out gmon_b.out
-gprof ./npb_bt_b gmon_b.out > profile_b.txt
+gprof ./npb_bt_b gmon_b.out > profile_b_local_1.txt
+
+./npb_bt_b
+mv gmon.out gmon_b.out
+gprof ./npb_bt_b gmon_b.out > profile_b_local_2.txt
+
+./npb_bt_b
+mv gmon.out gmon_b.out
+gprof ./npb_bt_b gmon_b.out > profile_b_local_3.txt
+
+./npb_bt_b
+mv gmon.out gmon_b.out
+gprof ./npb_bt_b gmon_b.out > profile_b_local_4.txt
